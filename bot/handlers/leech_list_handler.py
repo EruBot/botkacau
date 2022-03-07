eebaa0a83fa8ae4927851403256559bbb7131df4
UserLazy <78595134+UserLazy@users.noreply.github.com>
2022-03-07 17:13:00 +0700

    Update leech_list_handler.py

--- bot/handlers/leech_list_handler.py
+++ bot/handlers/leech_list_handler.py
@@ -10,7 +10,7 @@ LOGGER = logging.getLogger(__name__)
 
 from os.path import join as os_path_join
 from pyrogram import Client, filters
-from pyrogram import Message
+from pyrogram.types import Message
 from aria2p import Download
 from bot import LOCAL, STATUS, CONFIG, COMMAND
 from bot.plugins import aria2
